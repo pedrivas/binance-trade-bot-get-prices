de3852c5e6f9d766feee368cd2155277a3dc7674
Pedro <drivaspedro@gmail.com>
2022-03-20 21:57:12 -0300

    spring project files

---
+++ src/main/java/com/prices/binance/BinanceApplication.java
@@ -0,0 +1,13 @@
+package com.prices.binance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prices/binance/BinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prices.binance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
